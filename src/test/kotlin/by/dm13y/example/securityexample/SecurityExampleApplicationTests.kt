dfec55b57d1eb88fd99df316f5727a1f5b32ba9d
DmitrievichD <d.dmitrievich@itransition.com>
2020-02-07 21:57:50 +0300

    init

---
+++ src/test/kotlin/by/dm13y/example/securityexample/SecurityExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package by.dm13y.example.securityexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SecurityExampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
